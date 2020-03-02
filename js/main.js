fabf383987bc6c9fe3f519381a2453ce46856ed4
valer13572@gmail.com <46524230+Desonancen@users.noreply.github.com>
2020-03-02 13:11:58 +0200

    mobile fix

--- js/main.js
+++ js/main.js
@@ -49,3 +49,20 @@ $(function() {
     });
 
 });
+
+window.onload = function () {
+    hideAddressBar();
+    window.addEventListener("orientationchange", function () {
+        hideAddressBar();
+    }, false);
+}
+
+function hideAddressBar() {
+    setTimeout(function () {
+        document.body.style.height = window.outerHeight + 'px';
+        setTimeout(function () {
+            window.scrollTo(0, 1);
+        }, 1100);
+    }, 1000);
+    return false;
+}
